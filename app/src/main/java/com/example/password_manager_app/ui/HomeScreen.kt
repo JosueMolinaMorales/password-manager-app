822c9a21755868c9a6a29f6d81d4a486ace4d741
Andy Stoerkel <astoer1@students.towson.edu>
2022-10-30 18:23:18 -0400

    done home screen

--- app/src/main/java/com/example/password_manager_app/ui/HomeScreen.kt
+++ app/src/main/java/com/example/password_manager_app/ui/HomeScreen.kt
@@ -1,26 +1,41 @@
 package com.example.password_manager_app.ui
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.password_manager_app.R
 
 @Composable
 fun HomeScreen(
     onNavigateToLogin: () -> Unit
 ) {
     Column(
-        modifier = Modifier.fillMaxWidth(),
-        horizontalAlignment = Alignment.CenterHorizontally
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+            .padding(75.dp)
+            .border(2.dp, Color.Blue),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
     ){
-        Text(text = "Home Screen")
-        Button(onClick = onNavigateToLogin) {
+        Text(text = "Password Manager", fontSize = 25.sp)
+        Image(
+            painter = painterResource(id = R.drawable.img),
+            contentDescription = ""
+        )
+        Button(modifier = Modifier.fillMaxWidth(), onClick = onNavigateToLogin) {
             Text("Login")
         }
-        Button(onClick = {}) {
+        Button(modifier = Modifier.fillMaxWidth(), onClick = {}) {
             Text("Register")
         }
     }
