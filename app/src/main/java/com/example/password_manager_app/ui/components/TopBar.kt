d730a731e01a2dcf91302bc31a576cd4fa8e03c7
Josue Morales <molinajosue92@hotmail.com>
2022-11-19 13:04:11 -0500

    add search bar

--- app/src/main/java/com/example/password_manager_app/ui/components/TopBar.kt
+++ app/src/main/java/com/example/password_manager_app/ui/components/TopBar.kt
@@ -1,25 +1,28 @@
 package com.example.password_manager_app.ui.components
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.outlined.Menu
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.unit.dp
 import com.example.password_manager_app.R
 import com.example.password_manager_app.ui.theme.TopBarOpal
 
@@ -30,26 +33,32 @@ fun TopBar(onNavIconClick: () -> Unit) {
             Row(
                 modifier = Modifier
                     .fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceBetween
+                horizontalArrangement = Arrangement.SpaceEvenly
             ) {
-                val focusManager = LocalFocusManager.current
-                PasswordManagerTextField(
-                    value = "",
-                    onValueChange = {},
-                    placeholder = { Text(text="Search") },
-                    modifier = Modifier
-                        .fillMaxWidth(.8F)
-                        .scale(.9F),
-                    leadingIcon = {
-                        Icon(Icons.Default.Search, "")
-                    },
-                    keyboardOptions = KeyboardOptions(imeAction = ImeAction.Search),
-                    keyboardActions = KeyboardActions(onSearch = { focusManager.clearFocus() })
-                )
-                Image(
-                    painter = painterResource(id = R.drawable.koalalogo),
-                    contentDescription = ""
-                )
+                Column(
+                    modifier = Modifier.fillMaxWidth(.7F)
+                ) {
+                    val focusManager = LocalFocusManager.current
+                    PasswordManagerTextField(
+                        value = "",
+                        onValueChange = {},
+                        placeholder = { Text(text="Search") },
+                        leadingIcon = {
+                            Icon(Icons.Default.Search, "")
+                        },
+                        keyboardOptions = KeyboardOptions(imeAction = ImeAction.Search),
+                        keyboardActions = KeyboardActions(onSearch = { focusManager.clearFocus() })
+                    )
+                }
+                Column(
+                    modifier = Modifier.fillMaxWidth(.6F),
+                    horizontalAlignment = Alignment.End
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.koalalogo),
+                        contentDescription = ""
+                    )
+                }
             }
         },
         navigationIcon = {
